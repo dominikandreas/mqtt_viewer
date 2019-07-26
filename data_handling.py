fd23c5ca1798301e3232d344170920d4ee6cf7ec
dominik andreas <admin@localhost>
2019-07-26 17:04:49 +0200

    some cleanup, bugfix, comments

--- data_handling.py
+++ data_handling.py
@@ -7,9 +7,8 @@ import traceback
 import pickle
 import jinja2
 import logging
-from paho.mqtt.client import Client as MQTTClient
+from paho.mqtt.client import MQTTMessage, Client as MQTTClient
 from apscheduler.schedulers.background import BackgroundScheduler
-
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 DB_PATH = ROOT + "/data.db"
@@ -47,11 +46,11 @@ class Database:
     def save(self):
         if self.path is not None:
             try:
-                with open(self.path+".tmp", "wb") as f:
+                with open(self.path + ".tmp", "wb") as f:
                     pickle.dump(self.db, f)
                 if os.path.isfile(self.path):
                     os.remove(self.path)
-                os.rename(self.path+".tmp", self.path)
+                os.rename(self.path + ".tmp", self.path)
             except:
                 traceback.print_exc()
 
@@ -60,8 +59,17 @@ class Database:
         del self.scheduler
 
 
+mqtt_success_codes = {
+    0: 'CONNACK_ACCEPTED',
+    1: 'CONNACK_REFUSED_PROTOCOL_VERSION',
+    2: 'CONNACK_REFUSED_IDENTIFIER_REJECTED',
+    3: 'CONNACK_REFUSED_SERVER_UNAVAILABLE',
+    4: 'CONNACK_REFUSED_BAD_USERNAME_PASSWORD',
+    5: 'CONNACK_REFUSED_NOT_AUTHORIZED'}
+
+
 class MQTTLogger:
-    def __init__(self, server, graph_def, db, port=1883, history_size=600, on_view_update=None):
+    def __init__(self, server, graph_def, db, port=1883, history_size=3600, on_view_update=None, id=None):
         self.db = db
         self.db["histories"] = self.db.get("histories", {})
         self.history_size = history_size
@@ -79,47 +87,57 @@ class MQTTLogger:
         self.client.on_message = self.on_message
         self.client.connect(server, port, 60)
         self.client.loop_start()
+        self.id = id
 
     @property
     def histories(self):
         return self.db["histories"]
 
     def update_resolutions(self, name, history):
+        # update resolution scales each using the finer resolution as input
         resolutions = self.resolutions
-        for (res, time_diff), (source, _) in zip(resolutions[1:], resolutions[:-1]):
-            source, data = history.get(source), history.get(res, [])
+        raw_id = self.resolutions[0][0]
+        # first, run the update for the raw data
+        self.on_view_update(name, raw_id, history[raw_id][-1], self.id)
+        for (target_res, time_diff), (source_res, _) in zip(resolutions[1:], resolutions[:-1]):
+            source_res, data = history.get(source_res), history.get(target_res, [])
             if len(data) > 0:
-                if source[-1][0] > data[-1][0]+time_diff*1000:
-                    new_data = [s for s in source if s[0] > data[-1][0]]
+                if source_res[-1][0] > data[-1][0] + time_diff * 1000:
+                    # new data for is all data from the finer resolution that is newer than the last entry
+                    new_data = [s for s in source_res if s[0] > data[-1][0]]
+                    # new data point is a tuple of the mean time and mean value (x, y) of this new data
                     data.append(tuple(np.mean(new_data, axis=0)))
-                    history[res] = data[-self.history_size:]
-                    self.on_view_update(name, res, history[res][-1])
+                    # crop away data points that are beyond the maximum history size
+                    history[target_res] = data[-self.history_size:]
+                    # run the view update on the last element that was added to this resolutions history
+                    self.on_view_update(name, target_res, history[target_res][-1], self.id)
             else:
-                history[res] = [source[0]]
-                self.on_view_update(name, res, history[res][-1])
+                history[target_res] = [source_res[0]]
+                self.on_view_update(name, target_res, history[target_res][-1], self.id)
 
     def log(self, **entries):
         for name, value in entries.items():
             history = self.histories.get(name, {})
             data = history.get("raw", [])
-            data.append([(time.time()+utc_offset)*1000, value])
+            data.append([(time.time() + utc_offset) * 1000, value])
             history["raw"] = data[-self.history_size:]
             self.update_resolutions(name, history)
             self.histories[name] = history
 
-    def on_message(self, client, userdata, msg):
+    @staticmethod
+    def parse_payload(payload, template: jinja2.Template) -> float:
+        data = json.loads(payload.decode("utf-8"))
+        return float(data if template is None else template.render(**data))
+
+    def on_message(self, client, userdata, msg: MQTTMessage):
         try:
-            data_str = str(msg.payload.decode("utf-8"))
-            data = json.loads(data_str.replace("nan", "'nan'"))
-            for name, entry in self.graph_def.items():
-                if msg.topic == entry["topic"]:
-                    template = entry["template"]
-                    self.log(**{name: float(data if template is None else template.render(**data))})
+            self.log(**{name: self.parse_payload(msg.payload, template=entry["template"])
+                        for name, entry in self.graph_def.items() if msg.topic == entry['topic']})
         except Exception as e:
             traceback.print_exc()
 
-    def on_connect(self, client, userdata, flags, rc):
-        print("Connected with result code " + str(rc))
+    def on_connect(self, client, userdata, flags, rc: int):
+        print("Connected with result code " + mqtt_success_codes.get(rc, "unknown"))
         for topic in self.topics:
             client.subscribe(topic)
 
@@ -129,8 +147,3 @@ class MQTTLogger:
 
         return [{"name": name, "data": [d for d in data[resolution] if d[0] >= t_min]}
                 for name, data in self.histories.items()]
-
-
-#def gauss_filter(data, weights=(0.1, 0.2, 0.4, 0.2, 0.1)):
-#    x, y = zip(*data)
-#    return list(zip(x, np.convolve([*y[:5][::-1], *y, *y[-5:][::-1]], weights, "same").tolist()[5:-5]))
